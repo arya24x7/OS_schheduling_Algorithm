4183f2c240ae9a75b3a7ad2e5d97b4148367b9ea
Nehaaav <94235956+Nehaaav@users.noreply.github.com>
2022-08-27 10:16:14 +0530

    Update schedulingAlgo.py

--- schedulingAlgo.py
+++ schedulingAlgo.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sjf,fcfs,priority
 
-print("")
+print("Non-preemptive Scheduling is a CPU scheduling technique the process takes the resource (CPU time) and holds it till the process gets terminated or is pushed to the waiting state.\n" 
+      "No process is interrupted until it is completed, and after that processor switches to another process.\n"
+      "Algorithms that are based on non-preemptive Scheduling are First Come First Serve(FCFS), Shortest Job first(SJF), and non-preemptive priority.")
 
 print("Choose the type of non pre-emptive algorithm you want to use")
 print("1.FCFS\n2.SJF\n3.PRIORITY")
